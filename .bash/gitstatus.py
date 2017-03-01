9cd5c9004b7ee28966a46d80ee9fb73ece560eb3
hemulin <mr.hemulin@gmail.com>
2017-03-01 22:06:45 +0100

    changed gitstatus to support python3 env

--- .bash/gitstatus.py
+++ .bash/gitstatus.py
@@ -1,113 +1,152 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
+"""This module defines a Print function to use with python 2.x or 3.x., so we can use the prompt with older versions of
+Python too
+
+It's interface is that of python 3.0's print. See
+http://docs.python.org/3.0/library/functions.html?highlight=print#print
+
+Shamelessly ripped from
+http://www.daniweb.com/software-development/python/code/217214/a-print-function-for-different-versions-of-python
+"""
+# change those symbols to whatever you prefer
+symbols = {'ahead of': 'A', 'behind': 'B', 'prehash': ':'}
+
+import sys
+import re
 from subprocess import Popen, PIPE
-import sys, os
-from re import finditer
-from json import dumps
 
-################################################################################
-########## Config ##############################################################
-################################################################################
+__all__ = ["Print"]
 
-symbols = {'ahead of': 'A', 'behind': 'B', 'prehash':':'}
+try:
+    Print = eval("print")  # python 3.0 case
+    python_version = 3
+    to_str = str
+except SyntaxError as e:
+    python_version = 2
+    to_str = unicode
+    D = dict()
+    try:
+        exec ("from __future__ import print_function\np=print", D)
+        Print = D["p"]  # 2.6 case
+    except SyntaxError:
+        def Print(*args, **kwd):  # 2.4, 2.5, define our own Print function
+            fout = kwd.get("file", sys.stdout)
+            w = fout.write
+            if args:
+                w(str(args[0]))
+                sep = kwd.get("sep", " ")
+                for a in args[1:]:
+                    w(sep)
+                    w(str(a))
+            w(kwd.get("end", "\n"))
+    finally:
+        del D
 
-################################################################################
-########## End - Config ########################################################
-################################################################################
 
-NOT_IN_REPO_STR = 'fatal: Not a git repository'
-CWD = os.getcwd()
+def get_tag_or_hash():
+    cmd = Popen(['git', 'describe', '--exact-match'], stdout=PIPE, stderr=PIPE)
+    so, se = cmd.communicate()
+    tag = '%s' % so.decode('utf-8').strip()
 
-def _die(msg = None, returncode = -1):
-	if msg is not None:
-		stream = sys.stdout if returncode == 0 else sys.stderr
-		print >> stream, msg
-	exit(returncode)
-def dump(o):
-	print dumps(o, indent=4)
+    if tag:
+        return tag
+    else:
+        cmd = Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE, stderr=PIPE)
+        so, se = cmd.communicate()
+        hash_name = '%s' % so.decode('utf-8').strip()
+        return ''.join([symbols['prehash'], hash_name])
 
-p = Popen(['git', 'symbolic-ref', 'HEAD'], stdout=PIPE, stderr=PIPE)
-stdoutdata, stderrdata = p.communicate()
-if NOT_IN_REPO_STR in stderrdata:
-	_die(None, 0)
-#branch = stdoutdata[11:].strip()
-branch = stdoutdata[11:-1]
 
-stdoutdata, stderrdata = Popen(['git', 'rev-parse',  '--show-toplevel'], stdout=PIPE, stderr=PIPE).communicate()
-if stderrdata != "":
-	_die("BAD -2", -2)
-gitRoot = stdoutdata.strip()
+def get_stash():
+    cmd = Popen(['git', 'rev-parse', '--git-dir'], stdout=PIPE, stderr=PIPE)
+    so, se = cmd.communicate()
+    stash_file = '%s%s' % (so.decode('utf-8').rstrip(), '/logs/refs/stash')
 
-stdoutdata, stderrdata = Popen(['git','status','--porcelain'], stdout=PIPE, stderr=PIPE).communicate()
-if 'fatal' in stderrdata:
-	_die("BAD -1", -1)
-#print repr(stdoutdata)
-data = [x.groupdict() for x in finditer(r"(?P<mode>(?P<m1>.)(?P<m2>.)) (?P<path>.+?)(?: -> (?P<otherpath>.+))?\n", stdoutdata)]
-#dump(data)
-staged = 0
-untracked = 0
-modefied = 0
-dirty = 0
-other = 0
-for item in data:
-	pathInDir = not os.path.relpath(os.path.join(gitRoot, item["path"]), CWD).startswith("..")
-	otherpathInDir = False
-	if item["otherpath"] is not None:
-		otherpathInDir = not os.path.relpath(os.path.join(gitRoot, item["otherpath"]), CWD).startswith("..")
-#	print pathInDir, otherpathInDir
-	if item["mode"] == "??":
-		if pathInDir:
-			untracked += 1
-	if item["m1"] in "MADRC" and item["m2"] in " MD":
-		staged += 1
-	if item["m1"] in " MARC" and item["m2"] in "MD":
-		if pathInDir:
-			modefied += 1
-	if item["m1"] in "DAU" and item["m2"] in "DAU":
-		if pathInDir or otherpathInDir:
-			dirty += 1
-#print "U%d S%d M%d D%d O%d" % (untracked, staged, modefied, dirty, other)
+    try:
+        with open(stash_file) as f:
+            return sum(1 for _ in f)
+    except IOError:
+        return 0
 
-clean = str(int(untracked + staged + modefied + dirty + other == 0))
 
+# `git status --porcelain --branch` can collect all information
+# branch, remote_branch, untracked, staged, changed, conflicts, ahead, behind
+po = Popen(['git', 'status', '--porcelain', '--branch'], env={'LC_ALL': 'C'}, stdout=PIPE, stderr=PIPE)
+stdout, stderr = po.communicate()
+if po.returncode != 0:
+    sys.exit(0)  # Not a git repository
+
+# collect git status information
+untracked, staged, changed, conflicts = [], [], [], []
+num_ahead, num_behind = 0, 0
+ahead, behind = '', ''
+branch = ''
 remote = ''
+status = [(line[0], line[1], line[2:]) for line in stdout.decode('utf-8').splitlines()]
+for st in status:
+    if st[0] == '#' and st[1] == '#':
+        if re.search('Initial commit on', st[2]):
+            branch = st[2].split(' ')[-1]
+        elif re.search('no branch', st[2]):  # detached status
+            branch = get_tag_or_hash()
+        elif len(st[2].strip().split('...')) == 1:
+            branch = st[2].strip()
+        else:
+            # current and remote branch info
+            branch, rest = st[2].strip().split('...')
+            if len(rest.split(' ')) == 1:
+                # remote_branch = rest.split(' ')[0]
+                pass
+            else:
+                # ahead or behind
+                divergence = ' '.join(rest.split(' ')[1:])
+                divergence = divergence.lstrip('[').rstrip(']')
+                for div in divergence.split(', '):
+                    if 'ahead' in div:
+                        num_ahead = int(div[len('ahead '):].strip())
+                        ahead = '%s%s' % (symbols['ahead of'], num_ahead)
+                    elif 'behind' in div:
+                        num_behind = int(div[len('behind '):].strip())
+                        behind = '%s%s' % (symbols['behind'], num_behind)
+                remote = ''.join([behind, ahead])
+    elif st[0] == '?' and st[1] == '?':
+        untracked.append(st)
+    else:
+        if st[1] == 'M':
+            changed.append(st)
+        if st[0] == 'U':
+            conflicts.append(st)
+        elif st[0] != ' ':
+            staged.append(st)
 
-if not branch: # not on any branch
-	branch = symbols['prehash']+ Popen(['git','rev-parse','--short','HEAD'], stdout=PIPE).communicate()[0][:-1]
+stashed = get_stash()
+if not changed and not staged and not conflicts and not untracked and not stashed:
+    clean = 1
 else:
-	remote_name = Popen(['git','config','branch.%s.remote' % branch], stdout=PIPE).communicate()[0].strip()
-	if remote_name:
-		merge_name = Popen(['git','config','branch.%s.merge' % branch], stdout=PIPE).communicate()[0].strip()
-	else:
-		remote_name = "origin"
-		merge_name = "refs/heads/%s" % branch
-
-	if remote_name == '.': # local
-		remote_ref = merge_name
-	else:
-		remote_ref = 'refs/remotes/%s/%s' % (remote_name, merge_name[11:])
-	revgit = Popen(['git', 'rev-list', '--left-right', '%s...HEAD' % remote_ref],stdout=PIPE, stderr=PIPE)
-	revlist = revgit.communicate()[0]
-	if revgit.poll(): # fallback to local
-		revlist = Popen(['git', 'rev-list', '--left-right', '%s...HEAD' % merge_name],stdout=PIPE, stderr=PIPE).communicate()[0]
-	behead = revlist.splitlines()
-	ahead = len([x for x in behead if x[0]=='>'])
-	behind = len(behead) - ahead
-	if behind:
-		remote += '%s%s' % (symbols['behind'], behind)
-	if ahead:
-		remote += '%s%s' % (symbols['ahead of'], ahead)
+    clean = 0
 
 if remote == "":
-	remote = '.'
+    remote = '.'
+
+if python_version == 2:
+    remote = remote.decode('utf-8')
 
 out = '\n'.join([
-	str(branch),
-	str(remote),
-	str(staged),
-	str(dirty),
-	str(modefied),
-	str(untracked),
-	str(other),
-	clean])
-print(out)
+    branch,
+    remote,
+    to_str(len(staged)),
+    to_str(len(conflicts)),
+    to_str(len(changed)),
+    to_str(len(untracked)),
+    to_str(stashed),
+    to_str(clean),
+    to_str(python_version),
+])
+
+
+if python_version == 2:
+    Print(out.encode('utf-8'))
+else:
+    Print(out)
